65639b909c28f7cbbbb04c52968509b4bbb4df72
Jhonatas Machado <jhonatasrm@gmail.com>
2021-11-28 18:40:46 -0300

    adds dark and light icons

--- src/js/background.js
+++ src/js/background.js
@@ -1,9 +1,13 @@
 var arrayTabsIds = [];
 var tabs = '';
-var status = "true";
 var myStorage = window.localStorage;
-var iconLocalOff = "../res/icons/privacy_off.png";
-var iconLocalOn = "../res/icons/privacy_on.png";
+myStorage.setItem("status", "false");
+
+/* gets current theme to set toolbar icon */
+getCurrentThemeInfo();
+
+/* calls when the theme is changed */
+browser.theme.onUpdated.addListener(getCurrentThemeInfo);
 
 /* query for opened tabs */
 let querying = browser.tabs.query({});
@@ -26,19 +30,7 @@ browser.tabs.onRemoved.addListener(
     Tabs that are in the process of being closed cannot be hidden.
 */
 function actionClick() {
-  if(status == "true") {
-    browser.tabs.hide(arrayTabsIds);
-    status = "false";
-      browser.browserAction.setIcon({
-        path: iconLocalOn,
-      });
-  } else {
-    browser.tabs.show(arrayTabsIds);
-    status = "true";
-    browser.browserAction.setIcon({
-      path: iconLocalOff,
-    });
-  }
+  getCurrentThemeInfo(true);
 }
 
 function logTabs(tabs) {
@@ -59,4 +51,37 @@ function handleRemoved(tabId, isOnRemoved) {
       arrayTabsIds.splice(i, 1); 
     }
   }
+}
+
+function getStyle(themeInfo) {
+  if(myStorage.getItem("status") == "true") {
+    browser.tabs.hide(arrayTabsIds);
+    myStorage.setItem("status", "false");
+      if(themeInfo.colors.frame.charAt(1) !== "f"){
+        browser.browserAction.setIcon({
+          path: "../res/icons/privacy_on_dark_icon.png",
+        });
+      }else {
+        browser.browserAction.setIcon({
+          path: "../res/icons/privacy_on_light_icon.png",
+        });
+      }
+  } else {
+    browser.tabs.show(arrayTabsIds);
+    myStorage.setItem("status", "true");
+    if(themeInfo.colors.frame.charAt(1) !== "f"){
+      browser.browserAction.setIcon({
+        path: "../res/icons/privacy_off_dark_icon.png",
+      });
+    }else {
+      browser.browserAction.setIcon({
+        path: "../res/icons/privacy_off_light_icon.png",
+      });
+    }
+  }
+}
+
+async function getCurrentThemeInfo(clicked) {
+  var themeInfo = await browser.theme.getCurrent();
+  getStyle(themeInfo);
 }
