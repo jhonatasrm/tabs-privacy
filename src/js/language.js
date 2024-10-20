76a9865018ee52bc2ddd87fc77213b08f19a08a4
Jhonatas Machado <jhonatasrm@gmail.com>
2024-10-20 14:41:35 -0300

    Performance Improvement and Readability

--- src/js/language.js
+++ src/js/language.js
@@ -1,3 +1,4 @@
+// Loops through all elements in the document that have the 'data-manifest' attribute
 document.querySelectorAll('[data-manifest]').forEach(element => {
     const manifestKey = element.dataset.manifest;
     if (manifestKey && Manifest[manifestKey]) {
@@ -5,8 +6,10 @@ document.querySelectorAll('[data-manifest]').forEach(element => {
     }
 });
 
+// Loops through all elements in the document that have the 'data-i18n' attribute
 document.querySelectorAll('[data-i18n]').forEach(element => {
     const i18nKey = element.dataset.i18n;
+    
     if (i18nKey) {
         const message = browser.i18n.getMessage(i18nKey);
         if (message) {
